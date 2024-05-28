9aaa2b8d8b8faab850af045f4299292a39423227
Ravi <samcollins9899@gmail.com>
2024-05-28 11:43:40 +0530

    feat:update typo fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default async function Home() {
     <>
       <div>
         {/* <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div> */}
-        <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-          <div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
+        <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
         </div>
         <div className="ml-[10%] mr-[10%] mt-5 flex justify-between">
           <h1 className="text-xl font-bold">ECOMMERCE</h1>
